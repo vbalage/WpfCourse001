9dfaf122c9670157251b6936f8f1b0300261a273
Balazs Viszoczki <viszoczki@gmail.com>
2016-09-15 20:36:36 +0200

    kezdeti verzio

---
+++ WpfCourse001/Exercise3.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfCourse001
+{
+    /// <summary>
+    /// Interaction logic for Exercise3.xaml
+    /// </summary>
+    public partial class Exercise3 : Window
+    {
+        public Exercise3()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WpfCourse001/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCourse001
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Button source = e.OriginalSource as Button;
+            if (source == null) return;
+
+            Window window = null;
+            switch (source.Name)
+            {
+                case "btn_001":
+                    window = new Exercise1();                             
+                    break;
+                case "btn_002":
+                    window = new Exercise2();
+                    break;
+                case "btn_003":
+                    window = new Exercise3();
+                    break;
+                case "btn_004":
+                    window = new Exercise4();
+                    break;
+                case "btn_005":
+                    window = new Exercise5();
+                    break;
+                case "btn_006":
+                    window = new Exercise6();
+                    break;                
+            }
+
+            if (window == null)
+                return;
+            
+            window.Show();
+            window.Focus();            
+        }
+    }
+}
